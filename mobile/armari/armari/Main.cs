b63fc0732d2721c8e466b7a1b449fdb3b87bc9d7
Miguel Lopez <miguellopezn95@gmail.com>
2019-03-14 01:44:11 -0400

    agh

--- mobile/armari/armari/Main.cs
+++ mobile/armari/armari/Main.cs
@@ -1,6 +1,7 @@
 ﻿using UIKit;
 using System.IO;
 using Foundation;
+using System;
 
 namespace armari
 {
@@ -15,6 +16,31 @@ namespace armari
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+
+            //save prev images
+            //string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            //var pics = Directory.EnumerateFiles("images/tmp");
+            //Console.WriteLine(String.Format("ARMARI -> Saving Pictures"));
+            //foreach (var pic in pics)
+            //{
+            //    Console.WriteLine(String.Format("ARMARI -> Saving {0}", pic));
+            //    //string filename = Path.Combine(pic);
+            //    UIImage image_ = UIImage.FromFile(pic);
+            //    string filename = Path.Combine(folderPath, pic.Split('/')[2]);
+            //    NSData image = image_.AsPNG();
+            //    NSError err = null;
+
+            //    try
+            //    {
+            //        image.Save(filename, false, out err);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        //logger.Error("File issue", ex.ToString());
+            //    }
+            //}
+
+
             mh.Init();
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
